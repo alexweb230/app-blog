abcfdc783441442d98dc6591a2ff15cf1b89ab49
tania <tani.zhelezniak@gmail.com>
2019-10-28 08:28:24 +0200

    server

--- src/app/main/article-list/article-list.component.ts
+++ src/app/main/article-list/article-list.component.ts
@@ -1,35 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ArticleService} from '../services/article.service';
 import {Observable} from 'rxjs/index';
 import {Articlelist} from '../models/articlelist';
 import {HeaderService} from '../../header/header.service';
 
 @Component({
-  selector: 'app-article-list',
-  templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+    selector: 'app-article-list',
+    templateUrl: './article-list.component.html',
+    styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent implements OnInit {
 
-  articles$: Observable<Articlelist[]>;
+    articles$: Observable<Articlelist[]>;
 
-  loadArticle: Array<Number>;
-
-
+    loadArticle: Array<Number>;
 
 
-  constructor(private articleService: ArticleService, private headerServise: HeaderService) { }
+    constructor(private articleService: ArticleService, private headerServise: HeaderService) {
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.articles$ = this.articleService.getArticleHttp();
+        this.articles$ = this.articleService.getArticleHttp();
 
-    this.headerServise.title.next('Welcome Ladies & Gents to Calm Website.');
+        this.headerServise.title.next('Welcome Ladies & Gents to Calm Website.');
 
-     this.loadArticle = new Array(13).fill(0).map((n, ind) => ind);
-
-
-
-  }
+        this.loadArticle = new Array(13).fill(0).map((n, ind) => ind);
+    }
 
 }
