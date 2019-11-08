371a9210ab7d398c34dcc4f867a32de665b6f892
tania <tani.zhelezniak@gmail.com>
2019-11-08 09:53:02 +0200

    server

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductListComponent, ProductAlertsComponent],
   imports: [
     CommonModule,
     MainRoutingModule
---
+++ src/app/main/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {products} from '../models/products';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    productList = products;
+
+    share() {
+        alert('hello');
+    }
+
+}
