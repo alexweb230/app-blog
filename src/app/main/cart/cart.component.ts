3a7a1aed8afabe4aa17a6ac03d66f58038d22211
tania <tani.zhelezniak@gmail.com>
2019-11-11 11:10:10 +0200

    server

--- src/app/main/cart/cart.component.ts
+++ src/app/main/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsService} from '../servises/products.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductsService) { }
+
+  cartItem;
+
+  clear;
 
   ngOnInit() {
+
+    this.cartItem = this.productService.getItems();
+
+    this.clear = this.productService.claerCard();
+
   }
 
 }
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,21 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductdetailComponent} from './productdetail/productdetail.component';
+import {CartComponent} from './cart/cart.component';
 
 const routes: Routes = [
     {
-      path: '',
-      component: ProductListComponent
+        path: '',
+        component: ProductListComponent
     },
     {
         path: 'product/:id',
         component: ProductdetailComponent
+    },
+
+    {
+        path: 'cart',
+        component: CartComponent
     }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {
+}
