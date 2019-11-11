b411e44fde013b020bcea266b9c07b0fb731c0d4
tania <tani.zhelezniak@gmail.com>
2019-11-11 10:34:12 +0200

    server

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,9 +5,10 @@ import { MainRoutingModule } from './main-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductdetailComponent } from './productdetail/productdetail.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [ProductListComponent, ProductAlertsComponent, ProductdetailComponent],
+  declarations: [ProductListComponent, ProductAlertsComponent, ProductdetailComponent, CartComponent],
   imports: [
     CommonModule,
     MainRoutingModule
